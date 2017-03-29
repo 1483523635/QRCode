f243d34d8d16981152f694f953eca45f5b221bb2
曲廉卿 <1483523635@qq.com>
2017-03-29 14:36:39 +0800

    Origin Source

---
+++ QRCode/二维码生成器/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ThoughtWorks.QRCode;
+namespace 二维码生成器
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            textBox1.Text = 1.ToString();
+            textBox2.Text = 2.ToString();
+            comboBox1.SelectedValue = 0;
+            comboBox2.SelectedValue = 0;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string content = richTextBox1.Text==""? "0":richTextBox1.Text;
+            string Encode = comboBox1.SelectedItem.ToString();
+            string level = comboBox2.SelectedItem.ToString();
+            int version = int.Parse(textBox1.Text);
+            int size = int.Parse(textBox2.Text);
+            //MessageBox.Show(content + " " + Encode + " " + level + " " + version + " " + size);
+            getQRCode code=new getQRCode();
+         Image image=  code.simpleQRCode(content);
+            pictureBox1.Width = image.Width;
+            pictureBox1.Height = image.Height;
+            pictureBox1.Image = image;
+        }
+
+     
+    }
+}
---
+++ QRCode/二维码生成器/getQRCode.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ThoughtWorks.QRCode.Codec;
+
+namespace 二维码生成器
+{
+    /// <summary>
+    /// 生成二维码的类
+    /// </summary>
+   public  class getQRCode
+    {
+       /// <summary>
+       /// 内容
+       /// </summary>
+        private string content;
+       /// <summary>
+       /// 编码方式
+       /// </summary>
+        private string encodeing;
+       /// <summary>
+       /// 容错级别
+       /// </summary>
+        private string level;
+       /// <summary>
+       /// 版本号
+       /// </summary>
+        private int version;
+       /// <summary>
+       /// 图片尺寸
+       /// </summary>
+        private int size;
+
+       /// <summary>
+       /// 简单的二维码生成器
+       /// </summary>
+       /// <param name="text">需要的内容</param>
+       /// <returns>二维码图片</returns>
+        public Image  simpleQRCode(string text)
+        {
+            QRCodeEncoder encoder=new QRCodeEncoder();
+            encoder.QRCodeErrorCorrect = QRCodeEncoder.ERROR_CORRECTION.L;
+            encoder.QRCodeEncodeMode = QRCodeEncoder.ENCODE_MODE.BYTE;
+            encoder.QRCodeVersion = 4;
+            encoder.QRCodeScale = 4;
+            Image image = encoder.Encode(text);
+            return image;
+        }
+    }
+}
